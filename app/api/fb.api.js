d89e0e08304dea6b8a21cf64dbd9de3566a47efe
Wouter Willaert <wouter.willaert@gmail.com>
2012-06-20 14:15:38 +0200

    fb image upload

--- app/api/fb.api.js
+++ app/api/fb.api.js
@@ -9,17 +9,24 @@ module.exports = function (app) {
   app.post('/api/me/photobooth', function(req, res) {
 
     var message = 'Just took a picture from the Yunify photobooth!',
-        base64Image = req.body.data.replace(/^data:image\/\w+;base64,/, '').replace('+', ' '),
-        imageData = new Buffer(base64Image, 'base64').toString('binary');
+        base64Data = req.body.data.replace(/^data:image\/jpeg;base64,/, '');
+        //imageData = new Buffer(base64Data, 'base64').toString('binary');
 
     // base64 uploading refused to work, so binary it is
     var enc = 'binary',
-        filepath = 'filepath',
-        filename = 'filename.png';
+        isoDate = new Date().toISOString(),
+        filepath = '/public/data/photo_' + isoDate + '.jpeg',
+        filename = 'photo_' + isoDate + '.jpeg';
 
+    // extract file extention
     var re = /(?:\.([^.]+))?$/,
         ext = re.exec(filename)[1],
         authKey = req.session.auth.facebook.accessToken;
+
+    var dataBuffer = new Buffer(base64Data, 'base64');
+    require('fs').writeFile('/public/data/out.jpeg', dataBuffer, function(err) {
+      console.log('fs:error', err);
+    });
 
     // set up body for the request
     var outputBits = [];
@@ -40,42 +47,48 @@ module.exports = function (app) {
     outputBits2.push('\r\n------------0xKhTmLbOuNdArY--\r\n');
     var output2 = outputBits2.join("");
 
-    // set up header for the request
-    var options = {
-      host: 'graph.facebook.com',
-      port: 443,
-      path: '/me/photos?access_token=' + authKey,
-      method: 'POST',
-      headers: {
-        'Content-Type'   : 'multipart/form-data; boundary=----------0xKhTmLbOuNdArY',
-        'Content-Length' : output0.length + imageData.length + output2.length
+    fs.readFile(filepath, function(err, imageData) {
+      if (err) {
+        console.log('500 Error reading the image form disk:', err);
+      } else {
+        // set up header for the request
+        var options = {
+          host: 'graph.facebook.com',
+          port: 443,
+          path: '/me/photos',
+          method: 'POST',
+          headers: {
+            'Content-Type'   : 'multipart/form-data; boundary=----------0xKhTmLbOuNdArY',
+            'Content-Length' : output0.length + imageData.length + output2.length
+          }
+        };
+
+        // listen to response form the Facebook servers.
+        var request = https.request(options, function(response) {
+          response.setEncoding('utf8');
+          response.on('data', function(chunk) {
+            console.log(chunk);
+          });
+          response.on('end', function() {
+            console.log('200 OK');
+          });
+          response.on('close', function() {
+            console.log('500 Premature closing of the Facebook upload response.');
+          });
+        });
+
+        // catch errors
+        request.on('error', function(err) {
+          console.log('500 Problem with uploading the picture to Facebook: ' + err);
+        });
+
+        // write/send the request.
+        request.write(output0);
+        request.write(imageData);
+        request.write(output2);
+        request.end();
       }
-    };
-
-    // listen to response form the Facebook servers.
-    var request = https.request(options, function(response) {
-      response.setEncoding('utf8');
-      response.on('data', function(chunk) {
-        console.log(chunk);
-      });
-      response.on('end', function() {
-        console.log('200 OK');
-      });
-      response.on('close', function() {
-        console.log('500 Premature closing of the Facebook upload response.');
-      });
     });
-
-    // catch errors
-    request.on('error', function(err) {
-      console.log('500 Problem with uploading the picture to Facebook: ' + err);
-    });
-
-    // write/send the request.
-    request.write(output0);
-    request.write(imageData);
-    request.write(output2);
-    request.end();
 
   });
 
